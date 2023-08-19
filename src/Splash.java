8b1061398aebfad75b86cefc78142d60b55cbf45
Sohan <Sohanoffice46@gmail.com>
2023-08-19 19:13:47 +0530

    JFrame Heading

--- src/Splash.java
+++ src/Splash.java
@@ -1,8 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame {
 
     Splash() {
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel heading = new JLabel("EMPLOYEE MANAGEMENT SYSTEM");
+        heading.setBounds(80, 30, 1200, 60);
+        add(heading);
 
         setSize(1170, 650);
         setLocation(200, 100);
