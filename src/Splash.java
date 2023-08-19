e63b45932242462420dc401b6e49ee0105fff431
Sohan <Sohanoffice46@gmail.com>
2023-08-19 18:55:38 +0530

    JFrame Initializing

---
+++ src/Splash.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Splash extends JFrame {
+
+    Splash() {
+
+        setSize(1170, 650);
+        setLocation(200, 100);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+
+}
