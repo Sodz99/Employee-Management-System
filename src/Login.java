4857bc2e8c29012fde37d994a77457a6e8c589a2
Sohan <Sohanoffice46@gmail.com>
2023-08-20 13:52:58 +0530

    Login Frame

---
+++ src/Login.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login() {
+
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+
+}
--- src/Splash.java
+++ src/Splash.java
@@ -10,11 +10,36 @@ public class Splash extends JFrame {
 
         JLabel heading = new JLabel("EMPLOYEE MANAGEMENT SYSTEM");
         heading.setBounds(80, 30, 1200, 60);
+        heading.setFont(new Font("serif", Font.PLAIN, 60));
+        heading.setForeground(Color.RED);
         add(heading);
+
+        JButton clickhere = new JButton("CLICK HERE TO CONTINUE");
+        clickhere.setBounds(400, 400, 300, 70);
+        clickhere.setBackground(Color.BLACK);
+        clickhere.setForeground(Color.WHITE);
+        add(clickhere);
+
 
         setSize(1170, 650);
         setLocation(200, 100);
         setVisible(true);
+
+        while (true) {
+            heading.setVisible(false);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
+
+            heading.setVisible(true);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
+        }
 
     }
 
